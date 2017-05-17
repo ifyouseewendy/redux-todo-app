8c9b5f56151f283ba48bb278186ddf4645c1c20e
Di Wen <ifyouseewendy@gmail.com>
2017-05-16 22:57:58 -0400

    Add index

--- src/index.js
+++ src/index.js
@@ -7,3 +7,25 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+import { createStore } from "redux";
+import todoApp from "./reducers";
+
+let store = createStore(todoApp);
+
+import { addTodo, toggleTodo, setVisibilityFilter, VisibilityFilter } from "./actions";
+
+console.warn(store.getState());
+
+let unsubscribe = store.subscribe(() => {
+  console.warn(store.getState());
+});
+
+store.dispatch(addTodo("Learn about actions"));
+store.dispatch(addTodo("Learn about reducers"));
+store.dispatch(addTodo("Learn about store"));
+store.dispatch(toggleTodo(0));
+store.dispatch(toggleTodo(1));
+store.dispatch(setVisibilityFilter(VisibilityFilter.SHOW_COMPLETED));
+
+unsubscribe();
