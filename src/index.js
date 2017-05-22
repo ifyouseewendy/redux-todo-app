91cd0eaed3f785e160fe64dcc2521fa05dab1bd1
Di Wen <ifyouseewendy@gmail.com>
2017-05-22 12:44:56 -0400

    Update skeleton index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
+import todoApp from './reducers';
 import './index.css';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
-
-import { createStore } from "redux";
-import todoApp from "./reducers";
 
 let store = createStore(todoApp);
 
-import { addTodo, toggleTodo, setVisibilityFilter, VisibilityFilter } from "./actions";
-
-console.warn(store.getState());
-
-let unsubscribe = store.subscribe(() => {
-  console.warn(store.getState());
-});
-
-store.dispatch(addTodo("Learn about actions"));
-store.dispatch(addTodo("Learn about reducers"));
-store.dispatch(addTodo("Learn about store"));
-store.dispatch(toggleTodo(0));
-store.dispatch(toggleTodo(1));
-store.dispatch(setVisibilityFilter(VisibilityFilter.SHOW_COMPLETED));
-
-unsubscribe();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
